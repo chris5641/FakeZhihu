7900caa4564100f180d873beca27b664bafa6dd9
Chris.Wang <wangchong9139@gmail.com>
2018-05-04 17:40:48 +0800

    fix

--- fakeZhihu/settings.py
+++ fakeZhihu/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')tvlu&&7fa4!mtlm^5%9536*cn=)9zd1()v)evt7m()cf@!izu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.hi2world.cc']
 
 
 # Application definition
@@ -161,6 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'static/',
