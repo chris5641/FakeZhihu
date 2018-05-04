69bfe58ef062235da87fe779912cbc21d3d692af
Chris.Wang <wangchong9139@gmail.com>
2018-05-04 18:23:19 +0800

    fix

--- fakeZhihu/settings.py
+++ fakeZhihu/settings.py
@@ -161,9 +161,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'static/',
-]
 
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+        'static/',
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- users/views.py
+++ users/views.py
@@ -207,7 +207,6 @@ class FollowAskView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(FollowAskView, self).get_context_data(**kwargs)
-        context['asks_list'] = self.request.user.follow_asks.all().order_by('-create_time')
         context['FollowAskView'] = True
         return context
 
